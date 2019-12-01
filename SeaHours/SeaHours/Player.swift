71f477a9798858e750ec593c7b67809757044fb4
AYUMU <e185747@ie.u-ryukyu.ac.jp>
2019-12-01 17:46:34 +0900

    Player and  Chara commit. Player first commit.

--- SeaHours/SeaHours/Player.swift
+++ SeaHours/SeaHours/Player.swift
@@ -2,8 +2,30 @@
 //  Player.swift
 //  SeaHours
 //
-//  Created by 東谷　歩夢 on 2019/12/01.
+//  Created by Tomoya Gibo on 2019/12/01.
 //  Copyright © 2019 島袋太智. All rights reserved.
 //
 
 import Foundation
+
+class Player : Character {
+    private var maxMagicPoint: Int
+    private var nowMagicPoint: Int
+    private var item: [String] = [String]()
+    
+    init(name: String, maxHitPoint: Int, defense: Int, attack: Int, magickAttack: Int, maxMagicPoint: Int, nowMagicPoint: Int) {
+        self.maxMagicPoint = maxMagicPoint
+        self.nowMagicPoint = nowMagicPoint
+        
+        super.init(name: "プレイヤー", maxHitPoint: maxHitPoint, defense: defense, attack: attack, magickAttack: magickAttack)
+    }
+    
+    func getMagicPoint() -> Int {
+        return nowMagicPoint
+    }
+    
+    func magicPointOplate() -> Int {
+        //MPの増減
+        return 0
+    }
+}
