a3f4e70a5b059a4a0b5dee3f489802285edb2b58
AYUMU <e185747@ie.u-ryukyu.ac.jp>
2019-12-01 16:51:26 +0900

    make Skill class

---
+++ SeaHours/SeaHours/Skill.swift
@@ -0,0 +1,27 @@
+//
+//  Skill.swift
+//  SeaHours
+//
+//  Created by 東谷　歩夢 on 2019/12/01.
+//  Copyright © 2019 島袋太智. All rights reserved.
+//
+
+import Foundation
+
+class Skill {
+    var hitPercentageList: [Int]
+    
+    init(hitPercentageList: [Int]) { //コンストラクタ
+        self.hitPercentageList = hitPercentageList
+    }
+    
+    func nomalAttack(attack : Int) -> Int {
+        //命中率のパーセントをif文で書き，当たったらアタックの何倍？など適当に計算。外したら0を返す。
+        return 0
+    }
+    
+    func nomalMagickAttack(magickAttack : Int) -> Int {
+        //命中率のパーセントをif文で書き，当たったら魔法アタックの何倍？など適当に計算。外したら0を返す。
+        return 0
+    }
+}
