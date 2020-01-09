ce6ed52a75d4575fa32eaaf98c979fab38778e9f
AYUMU <e185747@ie.u-ryukyu.ac.jp>
2020-01-09 13:52:31 +0900

    commit fish

--- SeaHours/SeaHours/GameViewController.swift
+++ SeaHours/SeaHours/GameViewController.swift
@@ -12,25 +12,16 @@ import GameplayKit
 import AVFoundation
 
 class GameViewController: UIViewController {
-
+    
+    let sene=SceneManagement()
+    
+    @IBAction func test(_ sender: Any) {
+        sene.sceneTransition(corrent: self, sb: "battle", wi: "battle")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        if let view = self.view as! SKView? {
-//            // Load the SKScene from 'GameScene.sks'
-//            if let scene = SKScene(fileNamed: "GameScene") {
-//                // Set the scale mode to scale to fit the window
-//                scene.scaleMode = .aspectFill
-//
-//                // Present the scene
-//                view.presentScene(scene)
-//            }
-//
-//            view.ignoresSiblingOrder = true
-//
-//            view.showsFPS = true
-//            view.showsNodeCount = true
-//        }
     }
 //
 //    override var shouldAutorotate: Bool {
