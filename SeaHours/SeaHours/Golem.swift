551f7e1c52edb89171ee2b0c52aa2c6022b54030
Iori Chibana <e185746@ie.u-ryukyu.ac.jp>
2020-02-10 15:51:49 +0900

    2/10

---
+++ SeaHours/SeaHours/Golem.swift
@@ -0,0 +1,32 @@
+//
+//  Golem.swift
+//  SeaHours
+//
+//  Created by 知花　偉織 on 2020/02/10.
+//  Copyright © 2020 島袋太智. All rights reserved.
+//
+
+import Foundation
+class Golem : Enemy {
+    
+    //ステータス記述
+    init() {
+        super.init(name : "ゴレ" ,maxHitPoint: 50, defense: 150, attack: 5, magickAttack: 15)
+    }
+    var choseSkillName : String = ""
+    override func selectSkill() -> Double {
+        
+        var damege :Double
+        let randomNumber = Int.random(in: 1 ... 100)
+        if (60<=randomNumber){
+            damege = self.skill.nomalAttack(attack: attack, randomNumber: randomNumber)
+            choseSkillName = "攻撃"
+            return Double(damege)
+        }else{
+            damege = self.skill.stone(magickAttack: magickAttack, randomNumber: randomNumber)
+            choseSkillName = "ストーン"
+            return Double(damege)
+            
+        }
+    }
+}
