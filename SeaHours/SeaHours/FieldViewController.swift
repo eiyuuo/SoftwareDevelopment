9d22ad66c9adfd94ee975ef7d156f73372e56367
e185712 <e185712@ie.u-ryukyu.ac.jp>
2020-02-12 00:31:50 +0900

    一時的

--- SeaHours/SeaHours/FieldViewController.swift
+++ SeaHours/SeaHours/FieldViewController.swift
@@ -67,19 +67,27 @@ class FieldViewController: UIViewController {
         self.present(next, animated: true, completion: nil)
     }
     
+    @IBAction func UmiUsagi(_ sender:Any){
+        EnemyCount[5]=1
+        let storyboard = UIStoryboard(name: "battle", bundle: nil)
+        let next  = storyboard.instantiateViewController(withIdentifier:"VS") as! BattleViewController
+        next.enemyName = "UmiUsagi"
+        self.present(next, animated: true, completion: nil)
+        umiusagi.isEnabled = false
+    }
+
     
     
     @IBAction func golem(_ sender: Any) {
         let storyboard = UIStoryboard(name: "battle", bundle: nil)
         let next  = storyboard.instantiateViewController(withIdentifier:"VS") as! BattleViewController
         next.enemyName = "Golem"
+        next.count = timer.getCount()
         self.present(next, animated: true, completion: nil)
     }
     
     
     @IBAction func Status(_ sender: Any) {
-//        let scene = SceneManagement()
-//        scene.sceneTransition(corrent: self,sb: "PlayerStatus",wi: "Status")
         let storyboard = UIStoryboard(name: "PlayerStatus", bundle: nil)
         let next  = storyboard.instantiateViewController(withIdentifier:"Status") as! StatusViewController
         next.count = timer.getCount()
